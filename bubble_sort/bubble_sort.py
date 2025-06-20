54e07d5e94b028c3dc19215154550ad9469198fd
happy-wondessen <happywondessen@gmail.com>
2025-06-20 09:35:41 +0300

    initial commit

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+
+    for i in range(len(unsorted_list)):
+        for j in range(0, len(unsorted_list) - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                temp = unsorted_list[j]
+                unsorted_list[j] = unsorted_list[j+1]
+                unsorted_list[j + 1] = temp
+    return unsorted_list
+
+unsorted_list = [5, 3, 8, 4, 2]
+print(bubble_sort(unsorted_list))
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,16 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+
+    for i in range(len(arr)):
+        min = i
+        for j in range (i+1, len(arr)):
+            if arr[j] < arr[i]:
+                min = j
+        temp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = temp
+    return arr
+
+arr = [5, 3, 8, 4, 2]
+print(selection_sort(arr))
+
